c69399d98ba33cd77fdffdf5a441bdaa48b49a3b
Erik <e.naaru@hotmail.com>
2019-10-08 21:25:51 +0200

    Implement movable letter tiles, add good gitignore

---
+++ B-is-for-Battleship/Assets/Prefabs/LetterTile/LetterTile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LetterTile : MonoBehaviour {
+    public string Letter;
+    public Vector3 pickedOffset;
+
+    private TextMeshPro letterDisplay;
+    private static readonly int RAYCAST_IGNORE_LAYER = 2;
+    private static readonly int RAYCAST_DEFAULT_LAYER = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        letterDisplay = GetComponentInChildren<TextMeshPro>() ?? throw new MissingComponentException("No TextMeshPro found in LetterTile");
+        letterDisplay.text = Letter;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Select() {
+        transform.position = transform.position + pickedOffset;
+        gameObject.layer = RAYCAST_IGNORE_LAYER;
+    }
+
+    public void Delesect() {
+        transform.position = transform.position - pickedOffset;
+        gameObject.layer = RAYCAST_DEFAULT_LAYER;
+    }
+
+    public void Init(string letter) {
+        Letter = letter;
+    }
+}
---
+++ B-is-for-Battleship/Assets/Prefabs/TileTray/TileTray.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTray : MonoBehaviour {
+    private readonly List<LetterTile> playerHand = new List<LetterTile>();
+
+    public float tileOffsetY;
+    public float tileOffsetX;
+    public GameObject tileTray;
+
+    public int handSize;
+    public TilePile tilePile;
+
+    private void Start() {
+        FillHand();
+    }
+
+    public void AddTile(LetterTile tile, int pos) {
+        playerHand.Insert(pos, tile);
+        tile.transform.parent = tileTray.transform;
+
+        ReorderTiles();
+    }
+
+    public void MakeRoom(int pos) {
+        for (int i = 0; i < playerHand.Count; ++i) {
+            if( i != pos) {
+                Vector3 newPos = new Vector3((i - 3) * tileOffsetX, tileOffsetY, 0);
+                playerHand[i].transform.localPosition = newPos;
+            }
+        }
+    }
+
+    private void ReorderTiles() {
+        for(int i = 0; i < playerHand.Count; ++i) {
+            Vector3 newPos = new Vector3((i - 3) * tileOffsetX, tileOffsetY, 0);
+            playerHand[i].transform.localPosition = newPos;
+        }
+    }
+
+    private void FillHand() {
+        while (playerHand.Count < handSize) {
+            LetterTile newTile = tilePile.DrawTile();
+            newTile.transform.parent = transform;
+            playerHand.Add(newTile);
+        }
+        ReorderTiles();
+    }
+}
